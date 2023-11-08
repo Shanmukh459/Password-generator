d79b1d096880e51f4b439300f9cfb57416ab0629
Shanmukh459 <saikrrish459@gmail.com>
2023-11-08 13:52:17 -0800

    MVP

---
+++ index.js
@@ -0,0 +1,28 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordEl1 = document.getElementById("pwd1")
+let passwordEl2 = document.getElementById("pwd2")
+
+
+function generatePassword() {
+    let password1 = getPassword()
+    let password2 = getPassword()
+    
+    passwordEl1.innerText = password1
+    passwordEl2.innerText = password2
+}
+
+function getPassword() {
+    let tempPass= ""
+    for (let i = 0; i < 15; i++){
+        tempPass += characters[Math.floor(Math.random() * characters.length)]
+    }
+    return tempPass
+}
+
+
+
+
+
+
